78b9de71799ff882182e641bf7987faf7b2e7d16
Ubuntu <ubuntu@ip-172-31-89-146.ec2.internal>
2022-03-30 05:18:11 +0000

    .....

--- lifecare/models.py
+++ lifecare/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name=models.CharField(max_length=200)
+    email=models.EmailField(max_length=100)
+    day=models.CharField(max_length=20,null=True,blank=True)
+    date=models.DateField(max_length=100,null=True,blank=True)
+    doc=models.CharField(max_length=100,null=True,blank=True)
+    textarea_message=models.CharField(max_length=1000,null=True,blank=True)
--- lifecare/views.py
+++ lifecare/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
+
+from lifecare.models import Booking
 # Create your views here.
 
 def lifecare(request):
     return render(request,'index.html')
+
+
+def booking(request):
+    if request.method == 'GET':
+        return render(request, 'index.html')
+    else:
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        day = request.POST.get('day')
+        date = request.POST.get('date')
+        doc = request.POST.get('doc')
+        textarea_message = request.POST.get('textarea_message')
+        Booking.objects.create(name=name, email=email,day=day,date=date,doc=doc,textarea_message=textarea_message)
+        return redirect('lifecare:lifecare')
+
+
